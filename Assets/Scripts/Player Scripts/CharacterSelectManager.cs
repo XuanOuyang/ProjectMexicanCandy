ca9010206f09aa421e1ac73172ac5450c0229c9a
aodintsov1 <aodintsov4edu@gmail.com>
2026-08-21 22:35:13 -0700

    Transition character select input to gameplay controls
    Gameplay controls still controls both players at same time

--- Assets/Scripts/Player Scripts/CharacterSelectManager.cs
+++ Assets/Scripts/Player Scripts/CharacterSelectManager.cs
@@ -109,18 +109,6 @@ public class CharacterSelectManager : MonoBehaviour
         Debug.Log("Both players selected characters");
         Debug.Log($"Player 1: {player1.selectedCharacter}");
         Debug.Log($"Player 2: {player2.selectedCharacter}");
-        SwitchToGameplayInput(player1, "WASD");
-        SwitchToGameplayInput(player1, "Arrows");
         SceneManager.LoadScene("GrayBoxed Game");
-    }
-
-    private void SwitchToGameplayInput(LocalPlayer player, string controlScheme)
-    {
-        PlayerInput playerInput = player.GetComponent<PlayerInput>();
-        InputDevice[] devices = playerInput.devices.ToArray();
-        playerInput.actions = Instantiate(multiplayerInputActions);
-        playerInput.user.AssociateActionsWithUser(playerInput.actions);
-        playerInput.SwitchCurrentActionMap("Gameplay");
-        Debug.Log($"Player {player.playerNumber} switched to MultiplayerInput");
     }
 }
